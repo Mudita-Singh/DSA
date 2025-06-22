86eac8ef4189fa16017417bfee898c960f5151e6
Mudita <smudita900@gmail.com>
2025-06-23 00:23:31 +0530

    added new code

---
+++ src/java_codes/Searching_algorithm/Binary_Search4.java
@@ -0,0 +1,36 @@
+package java_codes.Searching_algorithm;
+
+// Floor of a number
+// Greatest number less than or equal to the target
+
+public class Binary_Search4 {
+    public static void main(String[] args) {
+        int[] arr = { 12 , 13 , 14, 16 ,17 , 28 , 22 , 23};
+        int target = 19;
+        int ans = search( arr , target);
+        System.out.println(ans);
+
+    }
+    static int search( int[] arr , int target){
+        int start = 0;
+        int end = arr.length-1;
+        while(end >= start ){
+            int mid = start +( end - start)/2;
+            if(target == arr[mid]){
+                return arr[mid];
+            }
+            else if(target >= arr[mid]){
+                start = mid + 1;
+            }
+            else{
+                end = mid-1;
+            }
+
+        }
+        return arr[end];
+    }
+
+
+}
+
+
