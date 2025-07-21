e26a5be4168f3904741bd8cd806dcd2e5d99dc8b
Mudita <smudita900@gmail.com>
2025-07-21 15:49:21 +0530

    added new codes

---
+++ src/java_codes/Bitwise_operators/Q4.java
@@ -0,0 +1,24 @@
+package java_codes.Bitwise_operators;
+
+public class Q4 {
+    public static void main(String[] args) {
+        int num = 6;
+        int base = 10;
+        search( num );
+
+       int ans = (int)(Math.log(num) / Math.log(base)) + 1 ;  // formula
+       System.out.println( ans );
+
+    }
+    public static void search(int num ){
+
+        int count = 0;    // works for binary base
+        while(num != 0){
+            count++;
+            num = num >> 1;
+        }
+
+        System.out.println(count);
+    }
+
+}
