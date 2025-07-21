5c2fc860b968cd4c7c1b2b0a2fec4ab979957278
Mudita <smudita900@gmail.com>
2025-07-21 22:21:40 +0530

    added codes

---
+++ src/java_codes/Bitwise_operators/Q7.java
@@ -0,0 +1,21 @@
+package java_codes.Bitwise_operators;
+
+public class Q7 {
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 10;
+        System.out.println( power(a, b) );
+    }
+
+    public static int power(int a, int b) {
+        int result = 1;
+        while (b > 0) {
+            if ((b & 1) == 1) {
+                result *= a;
+            }
+            a *= a;
+            b = b >> 1;
+        }
+        return result;
+    }
+}
