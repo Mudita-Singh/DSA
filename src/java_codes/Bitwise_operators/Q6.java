f9f777653d5386b1de3c3bc1df3a04bfc81ccc25
Mudita <smudita900@gmail.com>
2025-07-21 21:52:40 +0530

    added codes

---
+++ src/java_codes/Bitwise_operators/Q6.java
@@ -0,0 +1,35 @@
+package java_codes.Bitwise_operators;
+
+// find whether a number is in powers of 2
+
+public class Q6 {
+    public static void main(String[] args) {
+        int num = 16;
+
+        // method 1 using & operator
+        if((num & 1) == 0)
+            System.out.println("true");
+        else
+            System.out.println("false");
+
+        // method 2
+        search(num);
+
+    }
+
+    public static void search( int num ){
+       int count  = 0;
+       while(num != 0){
+           if ((num & 1) == 1) {
+               count++;
+           }
+           num = num >> 1;
+       }
+
+       if(count == 1)
+           System.out.println("true");
+       else
+           System.out.println("false");
+    }
+}
+
