ad2e0b29ac871be6e4e951e5e1855a85a3b82c98
Mudita <smudita900@gmail.com>
2025-07-22 17:12:12 +0530

    added codes

---
+++ src/java_codes/Bitwise_operators/Q8.java
@@ -0,0 +1,29 @@
+package java_codes.Bitwise_operators;
+
+public class Q8 {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 9;
+
+        int ans = xor(b)^xor(a-1);
+
+        System.out.println(ans);
+    }
+
+    public static int xor( int n ){
+        if( n % 4 == 0){
+            return n ;
+        }
+        if( n % 4 == 1){
+            return 1;
+        }
+        if( n % 4 == 2){
+            return n+1;
+        }
+        if( n % 4 == 3){
+            return 0;
+        }
+
+        return -1 ;
+    }
+}
