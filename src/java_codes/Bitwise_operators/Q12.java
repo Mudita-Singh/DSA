7b808e271a08ff4e1b778395c3d529077a5489d3
Mudita <smudita900@gmail.com>
2025-07-23 11:26:23 +0530

    made changes

--- src/java_codes/Bitwise_operators/Q12.java
+++ src/java_codes/Bitwise_operators/Q12.java
@@ -3,6 +3,8 @@ package java_codes.Bitwise_operators;
 // find factors of a number
 // more optimized way
 
+import java.util.ArrayList;
+
 public class Q12 {
     public static void main(String[] args) {
         find( 20);
@@ -10,15 +12,40 @@ public class Q12 {
 
     public static void find(int n){
 
+//        for( int i = 1 ; i <= Math.sqrt(n) ; i++){
+//            if( n%i == 0){
+//                if(n/i == i){
+//                    System.out.print(i + " ");  // duplicate value like for n = 36 , i = n/i = 6
+//                }
+//                else{
+//                    System.out.print(i + " " + n/i + " ");
+//                }
+//            }
+//        }
+//    }      // this doesn't print the factors in sorted order
+
+
+        // to print the factors in sorted order
+
+        ArrayList<Integer> list = new ArrayList<>();
+
         for( int i = 1 ; i <= Math.sqrt(n) ; i++){
             if( n%i == 0){
                 if(n/i == i){
                     System.out.print(i + " ");  // duplicate value like for n = 36 , i = n/i = 6
                 }
                 else{
-                    System.out.print(i + " " + n/i + " ");
+                    System.out.print(i + " " );
+                    list.add(n/i);
                 }
-            }
+           }
+        }
+
+        // print list
+        for( int i = list.size()-1 ; i >= 0 ; i--){
+            System.out.print(list.get(i) + " ");
         }
     }
 }
+
+
