43ca50cd83c850a7c4e270410e11a785289c74b2
Mudita <smudita900@gmail.com>
2025-07-24 20:54:12 +0530

    added new codes

--- src/java_codes/Recursion/Recursion17.java
+++ src/java_codes/Recursion/Recursion17.java
@@ -1,31 +1,28 @@
 package java_codes.Recursion;
 
 // print pattern using recursion
-
-// ****
-// ***
-// **
 // *
+// **
+// ***
+// ****
 
-public class Recursion16 {
+public class Recursion17 {
     public static void main(String[] args) {
 
-        find( 4 , 0 );  // 4 row left to be printed // 0 column/star printed
+        find(4, 0); // 4 row left to be printed // 0 column/star printed
     }
 
     public static void find( int row , int col){
         if( row == 0){
             return ;
         }
-
-        if( col < row){
-            System.out.print("* ");
+        if( col < row ){
             find(row , col+1);
+            System.out.print("* ");
         }
         else{
+            find(row-1 , 0);
             System.out.println( );
-            find( row-1 , 0);
-
         }
     }
 }
