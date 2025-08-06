680d57b6d800adb716a35d679e184f1248bd5708
Mudita <smudita900@gmail.com>
2025-08-06 19:46:32 +0530

    added new codes

---
+++ src/java_codes/Recursion/Recursion24.java
@@ -0,0 +1,20 @@
+package java_codes.Recursion;
+
+// print all the possible subsets of a string
+// using recursion
+
+public class Recursion24 {
+    public static void main(String[] args) {
+        subset("abc" , "");
+    }
+
+    public static void subset(String old , String stringNew){
+        if(old.isEmpty()){
+            System.out.println(stringNew);
+            return;
+        }
+
+        subset( old.substring(1) , stringNew + old.charAt(0)); // write string first otherwise if you write old.charAt(0) first then it will treat it as int value
+        subset(old.substring(1) , stringNew);
+    }
+}
