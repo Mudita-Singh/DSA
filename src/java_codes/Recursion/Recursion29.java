4722025f84250ee1f7041260117121da84969819
Mudita <smudita900@gmail.com>
2025-08-08 19:38:15 +0530

    added new codes

---
+++ src/java_codes/Recursion/Recursion29.java
@@ -0,0 +1,25 @@
+package java_codes.Recursion;
+
+// all the possible permutation from a string "abc"
+// using recursion
+
+public class Recursion29 {
+    public static void main(String[] args) {
+        permutation("" , "abc");
+    }
+    public static void permutation( String p , String up){
+        if(up.isEmpty()){
+            System.out.println(p);
+            return ;
+        }
+
+        char ch = up.charAt(0);
+
+        for( int i = 0 ; i <= p.length() ; i++){
+            String f = p.substring(0 , i);
+            String s = p.substring(i , p.length());
+
+            permutation(f + ch + s , up.substring(1));
+        }
+    }
+}
