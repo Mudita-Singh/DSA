b69e022166577c61a310d13568042531fc2be79f
Mudita <smudita900@gmail.com>
2025-09-16 16:41:05 +0530

    added new codes

--- src/java_codes/Basic_codes/Q2.java
+++ src/java_codes/Basic_codes/Q2.java
@@ -1,4 +1,31 @@
 package java_codes.Basic_codes;
 
-public class Q2 {
+public class Q2{
+    public static void main( String args[]){
+        String a = "mudita";
+
+        char[] arr = a.toCharArray();
+
+        int start = 0;
+        int end = arr.length - 1;
+
+        swap(start , end , arr);
+
+        String str = new String(arr);
+
+        System.out.println( str );
+
+    }
+
+    public static void swap( int start , int end , char[] arr){
+        while(start <= end){
+
+            char temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+
+            start++;
+            end--;
+        }
+    }
 }
