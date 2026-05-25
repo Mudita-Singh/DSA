c2e4484286841f248ea8f2d5137cc7bd641f870b
Mudita <smudita900@gmail.com>
2026-05-25 18:19:37 +0530

    added adjacency list code

---
+++ src/java_codes/Graph/Adjacency_List.java
@@ -0,0 +1,48 @@
+package java_codes.Graph;
+
+import java.util.ArrayList;
+
+public class Adjacency_List{
+
+    static class Edge {
+        int src;
+        int dest;
+
+        Edge(int src, int dest) {
+            this.src = src;
+            this.dest = dest;
+        }
+    }
+
+    public static void createGraph(ArrayList<Edge>[] graph) {
+
+        for(int i = 0; i < graph.length; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        graph[0].add(new Edge(0, 2));
+
+        graph[1].add(new Edge(1, 2));
+        graph[1].add(new Edge(1, 3));
+
+        graph[2].add(new Edge(2, 0));
+        graph[2].add(new Edge(2, 1));
+        graph[2].add(new Edge(2, 3));
+
+        graph[3].add(new Edge(3, 1));
+        graph[3].add(new Edge(3, 2));
+    }
+
+    public static void main(String[] args) {
+
+        int V = 4;
+
+        ArrayList<Edge>[] graph = new ArrayList[V];
+
+        createGraph(graph);
+
+        for(Edge e : graph[2]) {
+            System.out.print(e.dest + " ");
+        }
+    }
+}
